9af0468fa8c06e60386f890b22d0e8e6b4b1498f
Ashley Waudby <ashlinho07@gmail.com>
2024-06-27 11:53:40 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let hamburgerButton = document.querySelector('.hamburger').addEventListener('click', function(){
+    let flexNav = document.querySelector('.flex-nav');
+    flexNav.classList.toggle('show-nav')
+    
+})
+
